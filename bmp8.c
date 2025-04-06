1641d248c76b6d476f1791e02e4a4cc482d271c0
clarence <clarence.crozier@proton.me>
2025-04-06 18:29:45 +0200

    Ajout de fonctions et début du main

--- bmp8.c
+++ bmp8.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "bmp8.h"
 
-int bmp8_loadImage(const char * filename){
+t_bmp8 * bmp8_loadImage(const char * filename){
   File *fopen("lena_gray.bmp", "r");
-  fread();
+  t_bmp8* img = malloc(sizeof(t_bmp8));
+  return 0;
+}
+
+void bmp8_free(t_bmp8 * img){
+  free(img);
+}
+
+void bmp8_printInfo(t_bmp8 * img){
+  printf("Image info :");
+  printf("\n  Width : %d\n  Height : %d\n  Color Depth : %d\n  Date Size : %d", img->width, img->height, img->colorDepth, img->dataSize);
 }
--- main.c
+++ main.c
@@ -1,8 +1,65 @@
+#include "bmp8.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-    int a;
-    scanf("%d",&a);
-    printf("Hello, World!, %d\n", a);
+    t_bmp8 img;
+    int choix;
+    choix:
+    printf("Veuillez choisir une option :\n  1. Ouvrir une image\n  2. Sauvegarder une image\n  3. Appliquer un filtre\n  4. Afficher les informationds de l'image\n  5. Quitter\n");
+    scanf("%d", &choix);
+    if ((choix != 1) && (choix != 2) && (choix != 3) && (choix != 4) && (choix != 5)) {
+        do {
+            printf("Erreur, veuillez entrer un nombre correct : ");
+            scanf("%d", &choix);
+        } while ((choix != 1) && (choix != 2) && (choix != 3) && (choix != 4) && (choix != 5));
+    }
+    printf(">>> Votre choix : %d", choix);
+    char chemin[50];
+    if (choix == 1) {
+        printf("\nChemin du fichier : ");
+        scanf("%s", chemin);
+        printf("Image chargée avec succès !\n");
+        goto choix;
+    }
+    if (choix == 2) {
+        printf("\nChemin du fichier : ");
+        scanf("%s", chemin);
+        printf("Image sauvegardée avec succès !");
+        goto choix;
+    }
+    if (choix == 3) {
+        printf("Veuillez choisir un filtre :\n  1. Négatif\n    2. Luminosité\n 3. Binarisation\n   4. Flou\n   5. Flou gaussien\n  5. Netteté\n    6. Contours\n   7. Relief\n 8. Retourner au menu précédent");
+        scanf("%d", &choix);
+        if ((choix != 1) && (choix != 2) && (choix != 3) && (choix != 4) && (choix != 5) && (choix != 6) && (choix != 7) && (choix != 8)) {
+            do {
+                printf("Erreur, veuillez entrer un nombre correct : ");
+                scanf("%d", &choix);
+            } while ((choix != 1) && (choix != 2) && (choix != 3) && (choix != 4) && (choix != 5) && (choix != 6) && (choix != 7) && (choix != 8));
+        }
+        printf(">>> Votre choix : %d", choix);
+        if (choix == 1) {
+            bmp8_negative(t_bmp8 * img);
+        }
+        if (choix == 2) {
+            int value;
+            printf("Veuillez choisir une valeur : ");
+            scanf("%d", &value);
+            bmp8_brightness(t_bmp8 * img, value);
+        }
+        if (choix == 3) {
+            bmp8_threshold(t_bmp8 * img, 128);
+        }
+        if (choix == 4){}
+        if (choix == 5){}
+        if (choix == 6){}
+        if (choix == 7){}
+        goto choix;
+    }
+    if (choix == 4) {
+        bmp8_printInfo(t_bmp8 * img);
+        goto choix;
+    }
+
     return 0;
 }
